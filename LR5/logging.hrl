f294966050218e75d38c10adc697fd1f6166e87c
Podmaryov <86534896+Podmaryov@users.noreply.github.com>
2024-04-02 15:35:57 +0300

    Create logging.hrl

---
+++ LR5/logging.hrl
@@ -0,0 +1,12 @@
+
+-define(TRACE(Format, Data),
+    io:format("[TRACE] ~p ~p:  " ++ Format, [?MODULE, self()] ++ Data)).
+
+-define(INFO(Format, Data),
+    error_logger:info_msg("~p ~p:  " ++ Format, [?MODULE, self()] ++ Data)).
+
+-define(WARN(Format, Data),
+    error_logger:warning_msg("~p ~p:  " ++ Format, [?MODULE, self()] ++ Data)).
+
+-define(ERROR(Format, Data),
+    error_logger:error_msg("~p ~p:  " ++ Format, [?MODULE, self()] ++ Data)).
