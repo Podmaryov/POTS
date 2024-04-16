9d8f0e4b2cb46a14abbe56dfefdc4a86009bd782
Podmaryov <86534896+Podmaryov@users.noreply.github.com>
2024-04-16 14:48:16 +0300

    Add files via upload

---
+++ LR6/genserver_template.erl
@@ -0,0 +1,22 @@
+% Шаблон для реализации модулей обратного вызова gen_server.
+
+-module(genserver_template).
+
+% Сервер реализует поведение gen_server.
+
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+
+% Дополнительные вспомогательные функции, экспортируемые модулем % обратного вызова.
+
+-export([start/1]).
+
+start(Name) -> gen_server:start({local, Name}, ?MODULE, [], []).
+
+init([]) -> {ok, todo_state}.
+handle_call(_Request, _From, State) -> {reply, todo_reply, State}.
+handle_cast(_Msg, State) -> {noreply, State}.
+handle_info(_Info, State) -> {noreply, State}.
+terminate(_Reason, _State) -> ok.
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
